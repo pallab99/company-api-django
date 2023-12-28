f915d91d673ce47a07d970d1869709cfdb493602
Pallab Majumdar <kitkatpallab@gmail.com>
2023-12-28 20:25:45 +0600

    validation added for company model

--- company/models.py
+++ company/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from base.models import BaseModel
+from django.core.validators import MinLengthValidator
+
+
 # Create your models here.
 
 
 class Company(BaseModel):
-    name = models.CharField(max_length=50, unique=True, blank=False)
-    location = models.CharField(max_length=50, blank=False)
-    about = models.TextField(blank=False)
+    name = models.CharField(max_length=50, unique=True, blank=False, validators=[
+                            MinLengthValidator(5, "Name must be at least 5 characters")])
+    location = models.CharField(max_length=50, blank=False, validators=[
+        MinLengthValidator(5, "Location must be at least 5 characters")])
+    about = models.TextField(blank=False, validators=[
+        MinLengthValidator(10, "About must be at least 10 characters")])
     active = models.BooleanField(default=True, blank=False)
     type = models.CharField(max_length=50, blank=False)
     established = models.DateField(blank=False)
--- company/serializers.py
+++ company/serializers.py
@@ -1,12 +1,32 @@
+import re
 from rest_framework import serializers
 from .models import Company
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    def check_for_special_characters(self, value):
+        special_chars = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+        if special_chars.search(value):
+            raise serializers.ValidationError(
+                "should not contain special characters")
+        return value
+
     class Meta:
         model = Company
         fields = "__all__"
 
     def validate_name(self, name):
-        print("validation data", name)
+        name = self.check_for_special_characters(name)
         return name
+
+    def validate_location(self, location):
+        location = self.check_for_special_characters(location)
+        return location
+
+    def validate_about(self, about):
+        about = self.check_for_special_characters(about)
+        return about
+
+    def validate_type(self, type):
+        type = self.check_for_special_characters(type)
+        return type
