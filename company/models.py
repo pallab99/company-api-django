8ff0f26bd8210a1f85df2ca4c3fbc2405780a5bc
Pallab Majumdar <kitkatpallab@gmail.com>
2023-12-29 14:29:17 +0600

    added cache

--- company/models.py
+++ company/models.py
@@ -1,21 +1,61 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.utils.translation import gettext_lazy as _
 from base.models import BaseModel
-from django.core.validators import MinLengthValidator
-
-
-# Create your models here.
 
 
 class Company(BaseModel):
-    name = models.CharField(max_length=50, unique=True, blank=False, validators=[
-                            MinLengthValidator(5, "Name must be at least 5 characters")])
-    location = models.CharField(max_length=50, blank=False, validators=[
-        MinLengthValidator(5, "Location must be at least 5 characters")])
-    about = models.TextField(blank=False, validators=[
-        MinLengthValidator(10, "About must be at least 10 characters")])
-    active = models.BooleanField(default=True, blank=False)
-    type = models.CharField(max_length=50, blank=False)
-    established = models.DateField(blank=False)
+    """
+    Represents a company entity with various fields to store information about the company.
+    """
 
-    def __str__(self) -> str:
-        return str(self.name)
+    name = models.CharField(
+        verbose_name=_("Name"),
+        max_length=100,
+        unique=True,
+        blank=False,
+        validators=[MinLengthValidator(
+            5, _("Name must be at least 5 characters")), MaxLengthValidator(100, _("Name can not be greater than 100 characters"))]
+    )
+    location = models.CharField(
+        verbose_name=_("Location"),
+        max_length=100,
+        blank=False,
+        validators=[MinLengthValidator(
+            5, _("Location must be at least 5 characters")), MaxLengthValidator(100, _("Location can not be greater than 100 characters"))]
+    )
+    about = models.TextField(
+        verbose_name=_("About"),
+        blank=False,
+        validators=[MinLengthValidator(
+            10, _("About must be at least 10 characters"))]
+    )
+    active = models.BooleanField(
+        verbose_name=_("Active"),
+        blank=False,
+        default=True
+    )
+    company_type = models.CharField(
+        verbose_name=_("Type"),
+        max_length=100,
+        blank=False,
+        help_text=_("Type of the company"),
+        default="IT",
+        validators=[MinLengthValidator(
+            1, _("Company Type must be at least 1 characters")), MaxLengthValidator(100, _("Company Type can not be greater than 100 characters"))]
+    )
+    established = models.DateField(
+        verbose_name=_("Established"),
+        blank=False,
+        help_text=_("Date when the company was established")
+    )
+
+    class Meta:
+        verbose_name = _("Company")
+        verbose_name_plural = _("Companies")
+
+    def __str__(self):
+        """
+        Returns a string representation of the company object, which is its name.
+        """
+        return self.name
--- company/views.py
+++ company/views.py
@@ -1,24 +1,28 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from django.http import Http404
 from base.responseMessage import RESPONSE_MESSAGE
 from .models import Company
 from .serializers import CompanySerializer
 from rest_framework.decorators import action
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
 
+    @method_decorator(cache_page(60))
     def list(self, request, *args, **kwargs):
         response = super().list(request, *args, **kwargs)
         return Response(
-            {"message": RESPONSE_MESSAGE["get_all_data_success"],
+            {"success": True, "message": RESPONSE_MESSAGE["get_all_data_success"],
                 "data": response.data},
             status=status.HTTP_200_OK
         )
 
+    @method_decorator(cache_page(60))
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
@@ -36,6 +40,10 @@ class CompanyViewSet(viewsets.ModelViewSet):
                 {"success": False, "message": "Duplicate company", }, status=status.HTTP_409_CONFLICT
             )
         response = super().create(request, *args, **kwargs)
+        updated_queryset = self.get_queryset()
+        serialized_data = self.get_serializer(updated_queryset, many=True).data
+        cache.set('{}_list'.format(self.get_cache_key_prefix()),
+                  serialized_data, 60)
         return Response(
             {"message": "Company created successfully",
                 "data": response.data, }, status=status.HTTP_201_CREATED
@@ -43,6 +51,7 @@ class CompanyViewSet(viewsets.ModelViewSet):
 
     def update(self, request, *args, **kwargs):
         response = super().update(request, *args, **kwargs)
+        cache.delete('{}_list'.format(self.get_cache_key_prefix()))
         return Response(
             {"message": "Company updated successfully",
                 "data": response.data, "status": response.status_code},
@@ -50,19 +59,20 @@ class CompanyViewSet(viewsets.ModelViewSet):
 
     def destroy(self, request, *args, **kwargs):
         response = super().destroy(request, *args, **kwargs)
+        cache.delete('{}_list'.format(self.get_cache_key_prefix()))
         return Response(
             {"message": "Company deleted successfully",
-                "status": response.status_code},
+                "status": response.status_code, },
         )
 
     @action(detail=True, methods=["PATCH"], name="Toggle company status")
     def update_status(self, request, pk=None):
         try:
             companyObject = Company.objects.get(pk=pk)
-            print("Update status", companyObject)
             companyObject.active = not companyObject.active
             serializer = CompanySerializer(companyObject)
             companyObject.save()
+            cache.delete('{}_list'.format(self.get_cache_key_prefix()))
             return Response(
                 {"success": True, "message": "Company status updated successfully", "data": serializer.data})
 
@@ -73,3 +83,7 @@ class CompanyViewSet(viewsets.ModelViewSet):
         except Exception as e:
             return Response(
                 {"success": False, "message": "Internal Server Error", "error": str(e)})
+
+    def get_cache_key_prefix(self):
+        # Create a unique cache key prefix for this viewset
+        return 'company_viewset'
